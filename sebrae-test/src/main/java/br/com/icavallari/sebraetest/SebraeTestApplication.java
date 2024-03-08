e1bc330314a91beaa832225be1fab58be3c08777
icavallari <icavallari@hotmail.com>
2024-03-08 13:42:11 -0300

    feat: creating project

---
+++ sebrae-test/src/main/java/br/com/icavallari/sebraetest/SebraeTestApplication.java
@@ -0,0 +1,13 @@
+package br.com.icavallari.sebraetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SebraeTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SebraeTestApplication.class, args);
+	}
+
+}
---
+++ sebrae-test/src/test/java/br/com/icavallari/sebraetest/SebraeTestApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.icavallari.sebraetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SebraeTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
